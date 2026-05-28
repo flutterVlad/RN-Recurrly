b1120bb0927179b6e2ac702efd48a59b165b7450
Uladzislau Yurashevich <uladzislau.yurashevich@indev.by>
2026-05-28 14:00:22 +0300

    refactor: home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -27,71 +27,80 @@ export default function App() {
 
   const contentContainerStyle = useMemo(
     () => ({
-      paddingHorizontal: 16,
-      gap: 8,
       paddingBottom: insets.bottom + components.tabBar.height + 16,
+      gap: 8,
     }),
     [insets.bottom],
   );
 
+  const ListHeader = useMemo(
+    () => (
+      <>
+        <View className="home-header ml-4 mr-4 gap-4 mb-6">
+          <View className="rounded-full bg-muted p-2">
+            <User2 size={30} />
+          </View>
+          <Text className="flex-1 text-2xl font-sans-bold">
+            {HOME_USER.name}
+          </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Plus className="rounded border-2 border-b-black" />
+          </TouchableOpacity>
+        </View>
+
+        <View className="bg-accent rounded-tr-4xl rounded-bl-4xl p-4 flex-column gap-8 ml-4 mr-4 mb-6">
+          <Text className="text-xl text-white">Balance</Text>
+          <View className="flex-row justify-between items-center">
+            <Text className="text-white text-4xl font-sans-bold">
+              {formatCurrency(HOME_BALANCE.amount)}
+            </Text>
+            <Text className="text-card text-lg">
+              {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+            </Text>
+          </View>
+        </View>
+
+        <View className="pr-4 pl-4 mb-2">
+          <ListHeading title="Upcoming" />
+        </View>
+        <FlatList
+          data={UPCOMING_SUBSCRIPTIONS}
+          keyExtractor={(item) => item.id}
+          horizontal
+          style={{ flexGrow: 0 }}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 16, gap: 8 }}
+          renderItem={({ item }) => <UpcomingSubscriptionCard data={item} />}
+          ListEmptyComponent={
+            <Text className="font-sans-regular mt-4 mb-4 px-4 text-muted-foreground">
+              No upcoming subscription yet.
+            </Text>
+          }
+        />
+
+        <View className="pr-4 pl-4 mt-6 mb-2">
+          <ListHeading title="All Subscriptions" />
+        </View>
+      </>
+    ),
+    [],
+  );
+
   return (
-    <SafeAreaView edges={["top"]} className="flex-1 bg-background gap-6">
-      <View className="home-header ml-4 mr-4 gap-4">
-        <View className="rounded-full bg-muted p-2">
-          <User2 size={30} />
-        </View>
-        <Text className="flex-1 text-2xl font-sans-bold">{HOME_USER.name}</Text>
-        <TouchableOpacity onPress={() => {}}>
-          <Plus className="rounded border-2 border-b-black" />
-        </TouchableOpacity>
-      </View>
-
-      <View className="bg-accent rounded-tr-4xl rounded-bl-4xl p-4 flex-column gap-8 ml-4 mr-4">
-        <Text className="text-xl text-white">Balance</Text>
-
-        <View className="flex-row justify-between items-center">
-          <Text className="text-white text-4xl font-sans-bold">
-            {formatCurrency(HOME_BALANCE.amount)}
-          </Text>
-          <Text className="text-card text-lg">
-            {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
-          </Text>
-        </View>
-      </View>
-
-      <View className="pr-4 pl-4">
-        <ListHeading title="Upcoming" />
-      </View>
-      <FlatList
-        data={UPCOMING_SUBSCRIPTIONS}
-        keyExtractor={(item) => item.id}
-        horizontal
-        style={{ flexGrow: 0 }}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingHorizontal: 16,
-          gap: 8,
-        }}
-        renderItem={({ item }) => <UpcomingSubscriptionCard data={item} />}
-        ListEmptyComponent={
-          <Text className="font-sans-regular mt-4 mb-4 text-muted-foreground">
-            No upcoming subscription yet.
-          </Text>
-        }
-      />
-
-      <View className="pr-4 pl-4">
-        <ListHeading title="All Subscriptions" />
-      </View>
+    <SafeAreaView edges={["top"]} className="flex-1 bg-background">
       <FlatList
         data={HOME_SUBSCRIPTIONS}
-        className="flex-1"
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={contentContainerStyle}
-        renderItem={({ item }) => <SubscriptionCard data={item} />}
+        ListHeaderComponent={ListHeader}
+        renderItem={({ item }) => (
+          <View className="px-4">
+            <SubscriptionCard data={item} />
+          </View>
+        )}
         ListEmptyComponent={
-          <Text className="font-sans-regular mt-4 mb-4 text-muted-foreground">
+          <Text className="font-sans-regular mt-4 mb-4 px-4 text-muted-foreground">
             No subscription yet.
           </Text>
         }
